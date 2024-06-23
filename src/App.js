2b205af19ecac019f354c8181717428157e9dc15
DhruvGaur32 <154977139+DhruvGaur32@users.noreply.github.com>
2024-06-23 09:55:39 +0530

    added "task completed" option

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
-import React, { useState } from 'react'
-import "./App.css"
-import TodoInput from './components/TodoInput'
-import Todolist from './components/TodoList'
-// import Task from './components/task'
+import React, { useState } from 'react';
+import "./App.css";
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
 
 function App() {
-  const [listTodo,setListTodo]=useState([]);
-  let addList = (inputText)=>{
-    if(inputText!=='')
-      setListTodo([...listTodo,inputText]);
-  }
-  const deleteListItem = (key)=>{
-    let newListTodo = [...listTodo];
-    newListTodo.splice(key,1)
-    setListTodo([...newListTodo])
-  }
+  const [listTodo, setListTodo] = useState([]);
+
+  const addList = (inputText) => {
+    if (inputText !== '') {
+      setListTodo([...listTodo, { text: inputText, completed: false }]);
+    }
+  };
+
+  const deleteListItem = (index) => {
+    const newListTodo = listTodo.filter((_, i) => i !== index);
+    setListTodo(newListTodo);
+  };
+
+  const toggleComplete = (index) => {
+    const newListTodo = listTodo.map((item, i) => {
+      if (i === index) {
+        return { ...item, completed: !item.completed };
+      }
+      return item;
+    });
+    setListTodo(newListTodo);
+  };
+
   return (
     <div className="main-container">
       <div className="center-container">
-        <TodoInput addList={addList}/>
+        <TodoInput addList={addList} />
         <h1 className="app-heading">TODO</h1>
-        <hr/>
-        {listTodo.map((listItem,i)=>{
-          return (
-            <Todolist key={i} index={i} item={listItem} deleteItem={deleteListItem}/>
-          )
-        })}
-
-        
-        </div>
+        <hr />
+        <ul>
+          {listTodo.map((listItem, i) => (
+            <TodoList 
+              key={i} 
+              index={i} 
+              item={listItem} 
+              deleteItem={deleteListItem}
+              toggleComplete={toggleComplete}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-function Todolist(props) {
+function TodoList(props) {
   return (
-    <li className="list-item">
-        {props.item}
-        <span className='icons'>
-        <i className="fa-solid fa-trash-can icon-delete" 
-        onClick={e=>{
-            props.deleteItem(props.index)
-        }}></i>
-        </span>
+    <li className={`list-item ${props.item.completed ? 'completed' : ''}`}>
+      {props.item.text}
+      <span className="icons">
+        <i 
+          className="fa-solid fa-check icon-complete" 
+          onClick={() => props.toggleComplete(props.index)}
+        ></i>
+        <i 
+          className="fa-solid fa-trash-can icon-delete" 
+          onClick={() => props.deleteItem(props.index)}
+        ></i>
+      </span>
     </li>
-  )
+  );
 }
 
-export default Todolist
+export default TodoList;
